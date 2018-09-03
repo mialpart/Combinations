4d6adb080e77e6f686f920862d788e513bc474ef
Mikko Partanen <mikko.partanen93@gmail.com>
2018-09-04 01:12:02 +0300

    add timers

--- combination/Program.cs
+++ combination/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,50 +23,49 @@ namespace combination
             Console.WriteLine("\n --------------- \n");
             List<string[]> lists = new List<string[]>()
             {
-                new[]{"tyhjä", "varoitus"},
-                new[]{"tyhjäpieni","koulualue"},
+                new[]{"tyhjä", "varoitus", "mainos"},
+                new[]{"tyhjäpieni","koulualue", "mainos"},
                 new[]{"tyhjä", "rajoitus"},
             };
 
-            var cartesianLista = new string[]{ };
-            var num = lists[0].Count() * lists[1].Count() * lists[2].Count();
-            for (int i = 0; i < lists[0].Count(); i++)
-            {
-                for (int j = 0; j < lists[1].Count(); j++)
-                {
-                    for (int k = 0; k < lists[2].Count(); k++)
-                    {
-                        var uusiCartesianPari = new List<string>{lists[i][i], lists[j][j], lists[k][k] };
-                        //cartesianLista.Add(uusiCartesianPari);
-                        Console.WriteLine(uusiCartesianPari[0] + " " + uusiCartesianPari[1] + " " + uusiCartesianPari[2]);
-                    }
-                }
-            }
+
 
             Console.WriteLine("\n ------- Foreach ------- \n");
+            var listaCartesiani = new List<string[]>();
 
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
             foreach (var item in lists[0])
             {
-
                 foreach (var item1 in lists[1])
                 {
                     foreach (var item2 in lists[2])
                     {
-
-                        Console.WriteLine(item.ToString() + " " + item1.ToString() + " " + item2.ToString() + " ");
-
+                        var itemLista = new string[] { item.ToString(), item1.ToString() ,item2.ToString()};
+                        //Console.WriteLine(item.ToString() + " " + item1.ToString() + " " + item2.ToString() + " ");
+                        listaCartesiani.Add(itemLista);
                     }
                 }
             }
 
-            Console.WriteLine("\n -------------- \n");
+            foreach (var item in listaCartesiani)
+            {
+                Console.WriteLine(String.Join(" ", item));
+            }
+            sw.Stop();
+            Console.WriteLine("Loop time -> " + sw.Elapsed);
+            Console.WriteLine("\n ------- LINQ ------- \n");
 
+            sw.Restart();
             var cp = lists.CartesianProduct();
+            
 
             foreach (var line in cp)
             {
                 Console.WriteLine(String.Join(" ", line));
             }
+            sw.Stop();
+            Console.WriteLine("Loop time -> " + sw.Elapsed);
             Console.ReadKey();
         }
 
